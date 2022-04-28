663753d53c74d6e6cc79cd8fc732a9718647748f
Md Sakib Sarker <95316668+sakibsarker@users.noreply.github.com>
2022-04-28 15:24:22 +0600

    Create obj_newproperty.js
    JS object new property add code

---
+++ obj_newproperty.js
@@ -0,0 +1,11 @@
+let BillGates={
+    shirt:true,
+    shirtcolor:"white",
+    smile:true,
+    swatter:true,
+    swatterColor:"Gra",
+    Sunglass:true,
+};
+let newbill=Object.assign({},BillGates,{name:"sakib"});
+console.log("First obj: ",BillGates);
+console.log("2nd Obj",newbill);
